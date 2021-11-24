2cbceb1a0ca9a9d5b0d4298338a7e73fd8bb1011
Ursula.Netherland <ursula.netherland@bryantuniversity.edu>
2021-11-24 13:34:14 -0800

    add

---
+++ week3/rock-the-vote/models/user.js
@@ -0,0 +1,27 @@
+//import mongoose from 'mongoose'
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({ 
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("User", userSchema)
---
+++ week3/rock-the-vote/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+//  mongoose.connect('mongodb://localhost:27017/rock-the-vote',
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false
+//     },
+//     () => console.log("Connected to the DB")
+// )
+
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/rock-the-vote');
+  console.log("Connected to the DB")
+}
+
+app.use(express.json());
+
+// Routes
+app.use('/users', require('./routes/authRouter'))
+
+app.listen(9000,() => {
+ console.log("server listenting on port 9000.")
+})
